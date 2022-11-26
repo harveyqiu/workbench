57b955004467eb750f2b2e5535bba57af3c47b71
Harvey Qiu <qiushimao@hotmail.com>
2022-11-26 11:30:01 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # workbench
+
 ```
 workbench set "./20221123.md"
 
 workbench add "- [ ] Read Book"
 
 workbench cat
+```
+
+## Install
+```
+deno install --allow-read --allow-write --allow-env -n workbench index.ts
+
+export PATH="$HOME/.deno/bin:$PATH"
+
 ```
---
+++ index.ts
@@ -0,0 +1,41 @@
+import * as path from "https://deno.land/std@0.166.0/path/mod.ts";
+
+const [operation, content] = Deno.args;
+
+const home = Deno.env.get("HOME");
+
+async function getConfigData(home: string): Promise<string> {
+  const configPath = path.resolve(home, "workbench");
+  const configData = await Deno.readTextFile(configPath);
+  return configData;
+}
+
+if (home) {
+  switch (operation as string) {
+    case "set": {
+      const filePath = path.resolve(Deno.cwd(), content);
+
+      const configPath = path.resolve(home, "workbench");
+      await Deno.writeTextFile(configPath, filePath);
+
+      break;
+    }
+    case "cat": {
+      const configData = await getConfigData(home);
+
+      const workbenchData = await Deno.readTextFile(configData);
+      console.log(workbenchData);
+
+      break;
+    }
+    case "add": {
+      const configData = await getConfigData(home);
+      if (content) {
+        await Deno.writeTextFile(configData, content, { append: true });
+      }
+      break;
+    }
+  }
+} else {
+  console.log("Cannot get the home directory");
+}
